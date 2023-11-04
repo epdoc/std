181e94be400f10d1f3022fd6a44b29dc3d7a33a3
Jim Pravetz <jpravetz@gmail.com>
2023-11-03 18:14:15 -0600

    Revert back to using npm, dishing bun.

--- src/date-util.ts
+++ src/date-util.ts
@@ -1,21 +1,47 @@
-import { isValidDate, pad, isString, isDate } from 'epdoc-util';
+import { Integer, isValidDate, pad } from 'epdoc-util';
 
 export type Minutes = number;
-export type JulianDate = number;
+/**
+ * An integer value representing the Julian Day.
+ * @see [Julian day](https://en.wikipedia.org/wiki/Julian_day)
+ */
+export type JulianDay = Integer;
+/**
+ * A floating point number representing date and time, suitable for use in Google Sheets.
+ */
 export type GoogleSheetsDate = number;
 
-export function dateUtil(date?: Date) {
+/**
+ * Calls and returns `new DateUtil(date)`.
+ * @param date
+ * @returns
+ */
+export function dateUtil(date?: Date | string | Integer) {
   return new DateUtil(date);
 }
 
+/**
+ * A wrapper for a javascript `Date` object.
+ */
 export class DateUtil {
   private _date: Date;
   private _invalidDateString = 'Invalid Date';
 
-  constructor(date?: Date) {
-    this._date = isDate(date) ? new Date(date) : new Date();
+  /**
+   * Create a DateUtil object, which is a wrapper for a javascript `Date` object.
+   * @param date Optional Date object, or a string or number that can be used
+   * with the Date constructor method. If undefined then uses the value of `new Date()`.
+   */
+  constructor(date?: Date | string | Integer) {
+    this._date = date ? new Date(date) : new Date();
   }
 
+  /**
+   * Output the date in the form '2016-05-01T11:49:21-07:00'. This differs from
+   * `Date.toISOString` which always uses UTC in the output.
+   * @param showMs Set to false to hide (truncate) milliseconds
+   * @returns
+   */
   public toISOLocaleString(showMs: boolean = true): string {
     this.validate();
     const d = this._date;
@@ -38,25 +64,33 @@ export class DateUtil {
     return s;
   }
 
+  /**
+   * Validate whether the date is a valid Date object.
+   */
   private validate() {
     if (!isValidDate(this._date)) {
       throw new Error(this._invalidDateString);
     }
   }
 
-  public julianDate(): JulianDate {
+  /**
+   * Get the Julian Day.
+   * @returns A number which is the Julian Day
+   * @see [Julian day](https://en.wikipedia.org/wiki/Julian_day)
+   */
+  public julianDate(): JulianDay {
     this.validate();
     return Math.floor(this._date.getTime() / 86400000 + 2440587.5);
   }
 
-  private static tz(m: Minutes) {
+  public static tz(m: Minutes) {
     return (m < 0 ? '+' : '-') + pad(Math.abs(m) / 60, 2) + ':' + pad(Math.abs(m) % 60, 2);
   }
 
   /**
-   * Convert a javascript Date object to a date value used by Google Sheets.
-   * @param {*} jsDate A value that is passed to a Date constructor
-   * @returns
+   * Get the date in a Google Sheets value
+   * @returns A number which is the date with a value suitable for use in Google
+   * Sheets
    */
   public googleSheetsDate(): GoogleSheetsDate {
     this.validate();
--- src/duration-util.ts
+++ src/duration-util.ts
@@ -1,93 +1,165 @@
-import { deepCopy, isDict, isNonEmptyString, isString, pad } from 'epdoc-util';
+import {
+  Integer,
+  deepCopy,
+  isArray,
+  isBoolean,
+  isDefined,
+  isDict,
+  isInteger,
+  isNonEmptyString,
+  isString,
+  pad,
+} from 'epdoc-util';
 
-export type Milliseconds = number;
+const REG = {
+  formatName: new RegExp(/^(long|hms|:)$/),
+};
 
+export type Milliseconds = Integer;
+/**
+ * For `compact` mode this must be a string. Otherwise this can be a string or
+ * an array of one or two strings. If it is an array, the first string is used
+ * for singular values (_e.g. 1 day_) and the second string is used for plural
+ * values (_e.g. 2 days_).
+ *
+ * An example value is `[ 'hour', 'hours' ]`.
+ */
+export type FormatMsUnit = string | string[] | false;
+export function isFormatMsUnit(val: any): val is FormatMsUnit {
+  if (isString(val) || val === false) {
+    return true;
+  }
+  if (isArray(val) && isString(val[0]) && (val.length === 1 || isString(val[1]))) {
+    return true;
+  }
+  return false;
+}
+
+/**
+ * Defines the output format for the duration string.
+ */
 export type FormatMsOptions = {
-  d?: string;
-  h?: string;
-  m?: string; // set to a non string (false, null) to supress output, where compact is false
-  s?: string | false; // set to a non string (false, null) to supress output, where compact is false
-  ms?: string | false; // set to a non string (false, null) to supress output, where compact is false
-  decimal?: string | false;
+  /**
+   * Controls whether a compact or  long output format is used. With the compact
+   * format there are no spaces, and numbers padded to a standard width (_e.g._
+   * for minutes '2' becomes '02'). With the long format there are spaces and
+   * numbers are not padded. The long format is suitable for when the output is
+   * going to be read with text to speech, while the compact format is more
+   * suited to display.
+   */
   compact?: boolean;
+  /**
+   * The unit string to append to days.
+   */
+  d?: FormatMsUnit;
+  /**
+   * The unit string to append for hours.
+   */
+  h?: FormatMsUnit;
+  /**
+   * The unit string to append for minutes.
+   */
+  m?: FormatMsUnit;
+  /**
+   * The unit string to append for seconds.
+   */
+  s?: FormatMsUnit | false; // set to a non string (false, null) to supress output, where compact is false
+  /**
+   * The value `true` is the same as `3`, `false` is the same as `0`. Indicates
+   * the number of digits of milliseconds to display. For non-compact mode, this
+   * can only be a boolean `false` or a string and the string is appended to the
+   * output.
+   */
+  ms?: boolean | 0 | 1 | 2 | 3 | FormatMsUnit;
+  /**
+   * The character to use for a decimal place. Defaults to `.`.
+   */
+  decimal?: string | false;
+  /**
+   * A separator to use when `compact` is false.
+   */
   sep?: string;
 };
+const OPT_KEYS = ['d', 'h', 'm', 's', 'ms', 'compact', 'sep', 'decimal'];
 
 export function isFormatMsOptions(val: any): val is FormatMsOptions {
   if (
     isDict(val) &&
-    isString(val.d) &&
-    isString(val.h) &&
-    isString(val.m) &&
-    isString(val.s) &&
-    isString(val.ms)
+    (!isDefined(val.d) || isFormatMsUnit(val.d)) &&
+    (!isDefined(val.h) || isFormatMsUnit(val.h)) &&
+    (!isDefined(val.m) || isFormatMsUnit(val.m)) &&
+    (!isDefined(val.s) || isFormatMsUnit(val.s)) &&
+    (!isDefined(val.ms) || isFormatMsUnit(val.ms) || isBoolean(val.ms) || isInteger(val.ms))
   ) {
     return true;
   }
   return false;
 }
 
-export function durationUtil(ms: Milliseconds, opts?: FormatMsOptions | FormatName) {
+export function durationUtil(ms: Milliseconds, opts?: FormatMsOptions | FormatMsName) {
   return new DurationUtil(ms, opts);
 }
 
-export type FormatName = 'hms' | ':' | 'long';
-export function isFormatName(val: any): val is FormatName {
-  if (isNonEmptyString(val)) {
-    if (val === 'hms' || val === ':' || val === 'long') {
-      return true;
-    }
-  }
-  return false;
+/**
+ * Predefined output formats.
+ *
+ *  - `long` output format is `1 hour, 14 minutes, 3 seconds, 454 milliseconds`
+ *  - `hms` output format is `1h14m03.454s`
+ *  - `:` output format is `1:14:03.454`
+ */
+export type FormatMsName = 'hms' | ':' | 'long';
+export function isFormatMsName(val: any): val is FormatMsName {
+  return REG.formatName.test(val);
 }
 
 export class DurationUtil {
   private static OPTS: Record<string, FormatMsOptions> = {
-    hms: { d: 'd', h: 'h', m: 'm', s: 's', ms: '', compact: true, decimal: '.' },
-    ':': { d: 'd', h: ':', m: ':', s: '', ms: '', compact: true, decimal: '.' },
+    hms: { d: 'd', h: 'h', m: 'm', s: 's', ms: true, compact: true, decimal: '.' },
+    ':': { d: 'd', h: ':', m: ':', s: '', ms: true, compact: true, decimal: '.' },
     long: {
-      d: 'day',
-      h: 'hour',
-      m: 'minute',
-      s: 'second',
-      ms: 'millisecond',
+      d: ['day', 'days'],
+      h: ['hour', 'hours'],
+      m: ['minute', 'minutes'],
+      s: ['second', 'seconds'],
+      ms: ['millisecond', 'milliseconds'],
       compact: false,
       sep: ', ',
       decimal: '.',
     },
   };
-  private _opts: FormatMsOptions = DurationUtil.OPTS[':'];
+  private _opts: FormatMsOptions;
   // private _showMs: boolean;
   private _decimal: string;
 
   private _ms: Milliseconds = 0;
 
   /**
-   *
-   * @param ms The duration we are outputing
-   * @param opts
+   * Construct a new `DurationUtil` instance. If `formatting` is not a
+   * `FormatMsName` then will initialize formatting with default `:` format.
+   * @param ms The duration we are outputing. We use the absolute value.
+   * @param formatting Defines the format.
+   * @see options
    */
-  constructor(ms: Milliseconds, formatting?: FormatMsOptions | FormatName) {
+  constructor(ms: Milliseconds, formatting?: FormatMsOptions | FormatMsName) {
     this._ms = ms;
-    if (isFormatName(formatting)) {
-      this.options(formatting);
-    } else {
-      this.options(':').options(formatting);
+    if (!isFormatMsName(formatting)) {
+      this.options(':');
     }
+    this.options(formatting);
   }
 
   /**
-   * Define a custom format by overwriting the default format.
-   * @param opts The name of one of the preset formatting options, or a
-   * Dictionary with entries from a FormatMsOptions object, which are then used
-   * to override the individual default values.
+   * Define a custom format by overwriting the already-set format options.
+   * @param opts The `FormatMsName` name of one of the preset formats, or a
+   * `FormatMsOptions` object, which are then used to override the individual
+   * values.
    * @returns this
    */
-  public options(formatting: FormatMsOptions | FormatName): DurationUtil {
-    if (isFormatName(formatting)) {
+  public options(formatting: FormatMsOptions | FormatMsName): DurationUtil {
+    if (isFormatMsName(formatting)) {
       this._opts = deepCopy(DurationUtil.OPTS[formatting]);
-    } else if (isDict(formatting)) {
-      Object.keys(DurationUtil.OPTS.long).forEach((key) => {
+    } else if (isFormatMsOptions(formatting)) {
+      OPT_KEYS.forEach((key) => {
         if (formatting.hasOwnProperty(key)) {
           this._opts[key] = formatting[key];
         }
@@ -96,14 +168,21 @@ export class DurationUtil {
     return this;
   }
 
-  public format(formatting?: FormatMsOptions | FormatName): string {
+  /**
+   * Formats the output string.
+   * @param formatting Same as per the `options` method. A format to be used
+   * when constructing the output.
+   * @returns The formatted output string.
+   */
+  public format(formatting?: FormatMsOptions | FormatMsName): string {
     this.options(formatting);
     let ms = this._ms;
     if (ms < 0) {
       ms = -ms;
     }
     const opts = this._opts;
-    if (!isString(opts.ms)) {
+    if (opts.ms === false || opts.ms === 0) {
+      // Round out the milliseconds
       ms = Math.round(ms / 1000) * 1000;
     }
     const time = {
@@ -113,15 +192,19 @@ export class DurationUtil {
       s: Math.floor(ms / 1000) % 60,
       ms: Math.floor(ms) % 1000,
     };
-
     if (opts.compact) {
       let res = opts.s;
-      if (isString(opts.ms)) {
+      if (opts.ms === true || opts.ms === 3 || isString(opts.ms)) {
         res = opts.decimal + pad(time.ms, 3) + opts.s;
+      } else if (opts.ms === false || opts.ms === 0) {
+        res = opts.s;
+      } else if (isInteger(opts.ms) && [1, 2, 3].includes(opts.ms)) {
+        ms = Math.round(ms / 10 ** (3 - opts.ms));
+        res = opts.decimal + pad(ms, 3).slice(-opts.ms) + opts.s;
       }
       if (time.d) {
         return (
-          time.d +
+          String(time.d) +
           opts.d +
           pad(time.h, 2) +
           opts.h +
@@ -131,17 +214,25 @@ export class DurationUtil {
           res
         );
       } else if (time.h) {
-        return time.h + opts.h + pad(time.m, 2) + opts.m + pad(Math.floor(time.s), 2) + res;
+        return String(time.h) + opts.h + pad(time.m, 2) + opts.m + pad(Math.floor(time.s), 2) + res;
       } else if (time.m || !isNonEmptyString(opts.s)) {
-        return time.m + opts.m + pad(Math.floor(time.s), 2) + res;
+        return String(time.m) + opts.m + pad(Math.floor(time.s), 2) + res;
       }
-      return  String(time.s) + res;
+      return String(time.s) + res;
     } else {
       return Object.entries(time)
         .filter((val) => val[1] !== 0)
         .map(([key, val]) => {
-          if (isString(opts[key])) {
-            return `${val} ${opts[key]}${val !== 1 ? 's' : ''}`;
+          let units = opts[key];
+          if (isArray(units)) {
+            if (val !== 1 && units.length > 1) {
+              units = units[1];
+            } else {
+              units = units[0];
+            }
+          }
+          if (isString(units)) {
+            return `${val} ${units}`;
           }
         })
         .filter((val) => isNonEmptyString(val))
